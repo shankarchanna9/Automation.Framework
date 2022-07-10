7552eb9119d9b37531c6a0571e53a507f15a7457
shankarchanna9 <shankarchanna9@gmail.com>
2022-07-10 13:20:00 +0530

    added some files

---
+++ src/main/java/com/qa/test/loginPageTest.java
@@ -0,0 +1,20 @@
+package com.qa.test;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import com.qa.pages.loginPage;
+
+public class loginPageTest extends loginPage {
+
+	public loginPageTest(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Test
+	public void isLogin() {
+		// TODO Auto-generated method stub
+		super.login();
+	}
+}
